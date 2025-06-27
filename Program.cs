e5163b9b2a578692315f3fd52751cf7fc9726125
Chang <changz@live.com.au>
2025-06-27 16:40:53 +1000

    Working solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = Console.ReadLine();
+
+if (input != null) {
+    var numbers = input.Split(" ").Select(int.Parse).ToArray();
+    var n = numbers.Length + 1;
+    var sum = (n-1) * n / 2;
+
+    Console.WriteLine(sum - numbers.Sum());
+}
